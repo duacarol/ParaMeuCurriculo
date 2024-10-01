8d1453fcc9aab9351eee7010275249f098c40477
Carol <carolduarte2000@gmail.com>
2024-10-01 11:08:42 -0300

    add opção Adicionar Receita

--- CalculadoraFinanceira/Program.cs
+++ CalculadoraFinanceira/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<decimal> rctValor = new List<decimal>();
+List<string> rctDescricao = new List<string>();
+
+Console.WriteLine("Calculadora Financeira");
+Console.WriteLine("1- Adicionar Receita");
+Console.Write("Escolha uma opção: ");
+string opcao = Console.ReadLine();
+switch (opcao)
+{
+    case "1":
+        Console.Write("Insira o valor da receita: ");
+        decimal rctValorAdd = decimal.Parse(Console.ReadLine());
+        rctValor.Add(rctValorAdd);
+        Console.WriteLine(rctValor[0]);
+
+        Console.Write("Insira uma descrição para a receita: ");
+        string rctDescricaoAdd = Console.ReadLine();
+        rctDescricao.Add(rctDescricaoAdd);
+        Console.WriteLine(rctDescricao[0]);
+        break;
+
+    default:
+        Console.WriteLine("Opção inválida.");
+        break;
+}
