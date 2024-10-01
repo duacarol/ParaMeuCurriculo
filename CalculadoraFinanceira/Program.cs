d69708fadec51e535d95513cc64a14d497a3422c
Carol <carolduarte2000@gmail.com>
2024-10-01 11:15:14 -0300

    add opção Adicionar Despesa

--- CalculadoraFinanceira/Program.cs
+++ CalculadoraFinanceira/Program.cs
@@ -1,8 +1,11 @@
 ﻿List<decimal> rctValor = new List<decimal>();
 List<string> rctDescricao = new List<string>();
+List<decimal> dpsValor = new List<decimal>();
+List<string> dpsDescricao = new List<string>();
 
 Console.WriteLine("Calculadora Financeira");
 Console.WriteLine("1- Adicionar Receita");
+Console.WriteLine("2- Adicionar Despesa");
 Console.Write("Escolha uma opção: ");
 string opcao = Console.ReadLine();
 switch (opcao)
@@ -17,6 +20,18 @@ switch (opcao)
         string rctDescricaoAdd = Console.ReadLine();
         rctDescricao.Add(rctDescricaoAdd);
         Console.WriteLine(rctDescricao[0]);
+        break;
+
+    case "2":
+        Console.Write("Insira o valor da despesa: ");
+        decimal dpsValorAdd = decimal.Parse(Console.ReadLine());
+        dpsValor.Add(dpsValorAdd);
+        Console.WriteLine(dpsValor[0]);
+
+        Console.Write("Insira uma descrição para a despesa: ");
+        string dpsDescricaoAdd = Console.ReadLine();
+        dpsDescricao.Add(dpsDescricaoAdd);
+        Console.WriteLine(dpsDescricao[0]);
         break;
 
     default:
